8b6212bbdf24e7127440df3b383ac9a60b34dc03
Caio Vinicius Ribeiro de Mello <caio.mello@evalsaude.com.br>
2021-02-04 22:15:55 -0300

    Adicionando frontend

---
+++ frontend/src/data.js
@@ -0,0 +1,101 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Nootbook Dell Phonix 1TB',
+            category: 'Eletronics',
+            image: '/images/p1.jpg',
+            price: 3949,
+            brand: 'Dell',
+            rating: 5,
+            numReviews:13,
+            description: 'Produto de qualidade e que oferece segurança ao seu trabalho.',
+
+        },
+        {
+            _id: '2',
+            name: 'Iphone 12 Pro Max Apple 256GB',
+            category: 'Eletronics',
+            image: '/images/p2.jpg',
+            price: 7500,
+            brand: 'Apple',
+            rating: 5,
+            numReviews:10,
+            description: 'Rápido que é uma beleza, suas olhos vão adorar suas mãos também.',
+
+        },
+        {
+            _id: '3',
+            name: 'Fone Airpods com Estojo Apple',
+            category: 'Eletronics',
+            image: '/images/p3.jpg',
+            price: 1299,
+            brand: 'Apple',
+            rating: 4,
+            numReviews:18,
+            description: 'Som de qualidade que seus ouvidos irão te agradecer.',
+
+        },
+        {
+            _id: '4',
+            name: 'Caixa de Som JBL com Bluetooth',
+            category: 'Eletronics',
+            image: '/images/p4.jpg',
+            price: 2289,
+            brand: 'JBL',
+            rating: 4.5,
+            numReviews:11,
+            description: 'Máxima potência e qualidade você só tem com as caixas de som da JBL.',
+
+        },
+        {
+            _id: '5',
+            name: 'Smartwatch Relógio Inteligente',
+            category: 'Eletronics',
+            image: '/images/p5.jpg',
+            price: 259,
+            brand: 'HAYLOU',
+            rating: 4.5,
+            numReviews:19,
+            description: 'Tudo em um só relógio com tecnologia avançada.',
+
+        },
+        {
+            _id: '6',
+            name: 'Samsung Galaxy S20 128GB',
+            category: 'Eletronics',
+            image: '/images/p6.jpg',
+            price: 3299,
+            brand: 'Samsung',
+            rating: 4,
+            numReviews:17,
+            description: 'Smartphone do seu jeito.',
+
+        },
+        {
+            _id: '7',
+            name: 'MacBook Air Apple Intel Core i5',
+            category: 'Eletronics',
+            image: '/images/p7.jpg',
+            price: 5799,
+            brand: 'Apple',
+            rating: 5,
+            numReviews:12,
+            description: 'MacBook tem design leve e resistente e, com a bateria que dura até 12 horas, você pode fazer coisas incríveis o dia todo.',
+
+        },
+        {
+            _id: '8',
+            name: 'Imac Apple Intel I5 256GB',
+            category: 'Eletronics',
+            image: '/images/p8.jpg',
+            price: 9949,
+            brand: 'Apple',
+            rating: 5,
+            numReviews:10,
+            description: 'O Imac tem design leve e resistente e, com a bateria que dura até 12 horas, você pode fazer coisas incríveis o dia todo.',
+
+        },
+    ],
+};
+export default data;
