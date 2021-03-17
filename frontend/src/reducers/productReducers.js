62c50b0d8d8645d43c53221425c6faa97e9b3a87
Caio Vinicius Ribeiro de Mello <caio.mello@evalsaude.com.br>
2021-03-16 23:29:43 -0300

    Integrando Reducers.

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -3,7 +3,7 @@ const { PRODUCT_LIST_REQUEST,
         PRODUCT_LIST_FAIL,
      } = require("../constants/productConstants");
 
-const productListReducer = (state = { products: [] }, action) => {
+export const productListReducer = (state = { products: [] }, action) => {
     switch (action.type) {
         case PRODUCT_LIST_REQUEST:
             return { loading: true };
