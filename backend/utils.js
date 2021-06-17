90e151b108fac2d138c6a48856d85c966b1eafe8
Caio Ribeiro <caio.ribeiro11@hotmail.com>
2021-06-16 21:52:39 -0300

    Adicionando ordens de pedidos

--- backend/utils.js
+++ backend/utils.js
@@ -13,3 +13,24 @@ export const generateToken = (user) => {
     } 
   );
 };
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length); // O portador do token XXXXXX
+    jwt.verify(
+      token, 
+      process.env.JwT_SECRET || 'somethingsecret', 
+      (err, decode) => {
+      if (err) {
+        res.status(401).send({ message: 'Token Inválido' });
+       } else {
+        req.user = decode;
+        next();
+       }
+     }
+   );
+  } else {
+    res.status(401).send({ message: 'Token Não Encontrado' });
+  }
+};
